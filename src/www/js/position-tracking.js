77a085e5b00c3e614c4793b2df55c528ad711b08
Ruben Gamez <ruben.gamez@ed.ac.uk>
2015-09-08 11:28:07 +0100

    Add main module and tests skeleton

---
+++ src/www/js/position-tracking.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/* global cordova */
+
+define(function(require) {
+    var location = require('./location');
+    var PositionRecorder = require('./position-recorder');
+
+});
---
+++ src/www/js/tests.js
@@ -0,0 +1,6 @@
+'use strict';
+
+define(function(require) {
+    var systests = require('tests/systests');
+
+});
