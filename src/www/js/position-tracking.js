e0788fa8874bf8cf3934313b336a3324ff8c2561
Ruben Gamez <ruben.gamez@ed.ac.uk>
2015-09-21 11:17:41 +0100

    Add more, back, save and discard controls
    Issue edina/fieldtrip-cobweb-project#80

--- src/www/js/position-tracking.js
+++ src/www/js/position-tracking.js
@@ -20,25 +20,52 @@ define(function(require) {
 
     templates.editorItem = _.template(
         '<li class="editor" data-editor-id="<%=editorId%>" data-group-id="<%=groupId%>">' +
-            '<div class="recording">' +
-                '<div class="editor-label"><%-name%></div>' +
-                '<div class="controls">' +
-                    '<div class="control-button record off" data-action="start"></div>' +
-                    '<div class="control-button more" data-action="more"></div>' +
-                '</div>' +
+            '<div class="editor-label"><%-name%></div>' +
+            '<div style="display: none;" class="save-track">' +
+                '<input class="record-name" type="text" placeholder="Record name"/>' +
             '</div>' +
-            '<div class="saving" style="display: none">' +
-                '<div class="session">' +
-                    '<input type="text" />' +
-                '</div>' +
-                '<div class="controls">' +
-                    '<div class="control-button back" data-action="back"></div>' +
-                    '<div class="control-button add" data-action="add"></div>' +
-                    '<div class="control-button discard" data-action="discard"></div>' +
-                '</div>' +
+            '<div class="controls">' +
+                '<div class="control-button record off" data-action="start"></div>' +
+                '<div class="control-button more" data-action="more"></div>' +
+                '<div style="display: none;" class="control-button back" data-action="back"></div>' +
+                '<div style="display: none;" class="control-button save disabled" data-action="save"></div>' +
+                '<div style="display: none;" class="control-button discard disabled" data-action="discard"></div>' +
             '</div>' +
         '</li>'
     );
+
+    /**
+     * Goes through the list of editors and enable/disable the convenient controls
+     * according to the status of the recorders
+     */
+    var updateEditorControls = function(editor) {
+        var $editor = $(editor);
+        var $controls = $editor.find('.controls');
+        var editorId = $editor.data('editor-id');
+        var groupId = $editor.data('group-id');
+        var recorder = recorderManager.getRecorder(editorId, groupId);
+
+        if (recorder) {
+            if (recorder.isRecording()) {
+                $controls.find('.control-button.record')
+                    .removeClass('off')
+                    .addClass('on')
+                    .data('action', 'pause');
+            }
+            else {
+                $controls.find('.control-button.record')
+                .removeClass('on')
+                .addClass('off')
+                .data('action', 'start');
+            }
+        }
+        else {
+            $controls.find('.control-button.record')
+                .removeClass('on')
+                .addClass('off')
+                .data('action', 'start');
+        }
+    };
 
     /**
      * Render the list of editors and the recording controls
@@ -67,57 +94,105 @@ define(function(require) {
 
         $('#position-tracking-editors-list')
             .html(html)
+            .trigger('update-controls')
             .listview('refresh');
     };
 
     var registerControlEvents = function() {
+        $('#position-tracking-editors-list').on('input', 'input.record-name', function(event) {
+            $(event.target).removeClass('error');
+        });
 
-        var toggleRecordButton = function($control, recorder) {
-            var $recordButton = $control.find('.control-button.record');
+        $('#position-tracking-editors-list').on('update-controls', function() {
+            $(this)
+                .find('li.editor')
+                .each(function(index, editor) {
+                    updateEditorControls(editor);
+                });
+        });
 
-            if (recorder && recorder.isRecording()) {
-                $recordButton
-                    .removeClass('off')
-                    .addClass('on');
+        var changeEditorViewState = function($editor, state) {
+            var editorView = ['.editor-label', '.control-button.more', '.control-button.record'];
+            var savingView = ['.save-track', '.control-button.back', '.control-button.save', '.control-button.discard'];
+
+            switch (state) {
+                case 'saving-view':
+                    _(editorView).forEach(function(selector) {
+                        $editor.find(selector).hide();
+                    });
+                    _(savingView).forEach(function(selector) {
+                        $editor.find(selector).show();
+                    });
+                    break;
+                case 'list-view':
+                    _(editorView).forEach(function(selector) {
+                        $editor.find(selector).show();
+                    });
+                    _(savingView).forEach(function(selector) {
+                        $editor.find(selector).hide();
+                    });
+                    break;
             }
-            else {
-                $recordButton
-                    .removeClass('on')
-                    .addClass('off');
-            }
-        };
-
-        var getActionName = function($target) {
-            var controlTypes = ['record', 'pause', 'stop'];
-            var action;
-
-            action = _.find(controlTypes, function(className) {
-                return $target.hasClass(className);
-            });
-
-            return action;
         };
 
         $('#position-tracking-editors-list').on('vclick', '.control-button', function(event) {
             var recorder;
             var $targetButton = $(event.target);
-            var $controls = $targetButton.parent('.controls');
-            var editorId = $controls.data('editor-id');
-            var groupId = $controls.data('group-id');
+            var $editor = $targetButton.closest('li.editor');
+            var editorId = $editor.data('editor-id');
+            var groupId = $editor.data('group-id');
+            var action = $targetButton.data('action');
 
-            switch (getActionName($targetButton)) {
-                case 'record':
+            switch (action) {
+                case 'start':
                     recorder = recorderManager.startRecorder(editorId, groupId);
                     break;
                 case 'pause':
                     recorder = recorderManager.pauseRecorder(editorId, groupId);
                     break;
-                case 'stop':
-                    recorder = recorderManager.stopRecorder(editorId, groupId);
+                case 'more':
+                    changeEditorViewState($editor, 'saving-view');
+                    break;
+                case 'back':
+                    changeEditorViewState($editor, 'list-view');
+                    break;
+                case 'discard':
+                    recorder = recorderManager.disposeRecorder(editorId, groupId);
+                    changeEditorViewState($editor, 'list-view');
+                    break;
+                case 'save':
+                    var recordName = '';
+                    var $input = $editor.find('input.record-name');
+
+                    recordName = $input.val();
+                    if (recordName.length > 0) {
+                        $input.attr('disabled', 'disabled');
+                        recorderManager.saveRecorder(editorId, groupId, recordName);
+                        recorderManager.disposeRecorder(editorId, groupId);
+
+                        $targetButton.addClass('rotate');
+
+                        setTimeout(function() {
+                            changeEditorViewState($editor, 'list-view');
+
+                            $input
+                                .val('')
+                                .removeAttr('disabled');
+                            $targetButton.removeClass('rotate');
+                        }, 800);
+                    }
+                    else {
+                        $input
+                            .addClass('error')
+                            .focus();
+                    }
                     break;
             }
 
-            toggleRecordButton($controls, recorder);
+            $('#position-tracking-editors-list').trigger('update-controls');
+
+            event.stopPropagation();
+            return false;
         });
     };
 
@@ -134,8 +209,8 @@ define(function(require) {
         });
 
         $(document).on('pagebeforeshow', '#position-tracking-main-page', function() {
-            renderEditorsList();
             registerControlEvents();
+            renderEditorsList();
         });
     };
 
